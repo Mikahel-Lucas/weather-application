cbac3d9fca73664ce24de8e2a79ba864e4eb1cf9
Mikahel Gonçalves <mikahel800@gmail.com>
2024-01-01 11:53:12 -0300

    Saved the city searched by the user in the local storage, and refactored the code.

--- js/app.js
+++ js/app.js
@@ -7,34 +7,43 @@ const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
-const showCityWeatherInfo = async cityName => {
-    const [{ Key, LocalizedName }] = await getCityData(cityName)
-    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon}] = await 
-    getCityWeather(Key)
-    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-    
-    timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
-    timeIconContainer.innerHTML = timeIcon
-    const setCityData = (div, data) => div.textContent = data
-    setCityData(cityNameContainer, LocalizedName)
-    setCityData(cityWeatherContainer, WeatherText)
-    setCityData(cityTemperatureContainer, Temperature.Metric.Value)
-}
-
-
 const showCityCard = () => {
     if (cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
     }
 }
 
-cityForm.addEventListener('submit', async event => {
-    event.preventDefault()
-    
-    const inputValue = event.target.city.value
+const showCityWeatherInfo = async cityName => {
+    const [{ Key, LocalizedName }] = await getCityData(cityName)
+    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await
+        getCityWeather(Key)
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+    timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
+    timeIconContainer.innerHTML = timeIcon
+    cityNameContainer.textContent = LocalizedName
+    cityWeatherContainer.textContent = WeatherText
+    cityTemperatureContainer.textContent = Temperature.Metric.Value
 
     showCityCard()
+}
+
+const showLocalStorageCity = () => {
+    const city = localStorage.getItem('city')
+
+    if (city) {
+        showCityWeatherInfo(city)
+    }
+}
+
+cityForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const inputValue = event.target.city.value
+
     showCityWeatherInfo(inputValue)
+    localStorage.setItem('city', inputValue)
     cityForm.reset()
 })
 
+showLocalStorageCity()
